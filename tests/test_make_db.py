10cf80c8b5b0292ac2d3be1f7dd0b814d1c8f9d5
laowantong <laowantong@users.noreply.github.com>
2019-12-13 17:05:15 +0100

    amend last commit by renaming ignored file

--- tests/test_make_db.py
+++ tests/test_make_db.py
@@ -9,7 +9,7 @@ from paroxython import make_db
 def test_make_db():
     f = lambda d: {k: "ignored" for (k, v) in d.items() if k != "timestamp"}
     result = make_db.make_database(["tests/data/programs"])
-    expected = Path("tests/data/programs/db.json").read_text()
+    expected = Path("tests/data/programs/test_db.json").read_text()
     assert json.loads(result, object_hook=f) == json.loads(expected, object_hook=f)
 
 
