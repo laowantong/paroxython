a7ef5b11a8003864bf60294db63cda40002b6c3b
laowantong <aristide.grange@univ-lorraine.fr>
2019-12-16 15:58:37 +0100

    extend regex suggestions

--- helpers/suggest_regex.py
+++ helpers/suggest_regex.py
@@ -2,24 +2,61 @@ import regex
 import itertools
 
 FIRST_PREFIX_MASK = r"\n(?:\1.+\n)*?\1/"
-FIRST_PREFIX = " " * (len(FIRST_PREFIX_MASK) - len("^(.*?)/")) + "^(.*?)/"
+FIRST_PREFIX = " " * (len(FIRST_PREFIX_MASK) - len("^(.*)/")) + "^(.*)/"
 
 
 class Suggestion:
+    """Suggest a regex matching a selection of lines in a flat AST."""
+
     def __call__(self, source):
-        self.source = source
+        self.s = source
         self.abstract_first_longest_prefix()
         self.abstract_line_numbers()
-        return self.source
+        for key in ("name", "id", "_hash", "_ids"):
+            self.capture_and_match_key(key)
+        self.abstract_body_prefix()
+        return self.s
 
     def abstract_first_longest_prefix(self):
         rex = r"(?m)^(?P<PREFIX>.+)/(?P<SUFFIX>.+\n)(?:\1/(?P<SUFFIXES>.+\n))*"
-        match = regex.search(rex, self.source)
+        match = regex.search(rex, self.s)
         repl = [FIRST_PREFIX + match.captures("SUFFIX")[0]]
         for suffix in match.captures("SUFFIXES"):
             repl.append(FIRST_PREFIX_MASK + suffix)
-        repl = "".join(repl)
-        self.source = self.source[: match.start()] + repl + self.source[match.end() :]
+        self.s = self.s[: match.start()] + "".join(repl) + self.s[match.end() :]
 
     def abstract_line_numbers(self):
-        self.source = regex.sub(r"lineno=\d+\n", r"lineno=(?P<LINE>\d+)\n", self.source)
+        self.s = regex.sub(r"lineno=\d+\n", r"lineno=(?P<LINE>\d+)\n", self.s)
+
+    def capture_and_match_key(self, key):
+        rex = fr"(?ms)/(?P<KEY>{key})=(?P<VALUE>['\"\w_]+)((?P<SKIPS>\n.+?)(?P<KEYS>{key})=\2)+"
+        name = key.upper().replace("_", "")
+        for i in itertools.count(1):
+            match = regex.search(rex, self.s)
+            if not match:
+                break
+            d = match.capturesdict()
+            key = d["KEY"][0]
+            repl = [fr"/{key}=(?P<{name}_{i}>.+) # capture {key} #{i}"]
+            for (skip, key) in zip(d["SKIPS"], d["KEYS"]):
+                repl.append(fr"{skip}{key}=(?P={name}_{i}) # match {key} #{i}")
+            self.s = self.s[: match.start()] + "".join(repl) + self.s[match.end() :]
+        if fr"(?P={name}_2)" not in self.s:  # no need for numeric suffix
+            self.s = self.s.replace(fr"(?P<{name}_1>.+)", fr"(?P<{name}>.+)")
+            self.s = self.s.replace(fr"(?P={name}_1)", fr"(?P={name})")
+            self.s = self.s.replace(fr" {key} #1", fr" {key}")
+
+    def abstract_body_prefix(self):
+        rex = fr"(?m)^(?P<SKIP>.+?)/(?P<PREFIX>body/\d+)/(?P<SUFFIX>.+\n)(?:\1/\2/(?P<SUFFIXES>.+\n))+"
+        for i in itertools.count(1):
+            match = regex.search(rex, self.s)
+            if not match:
+                break
+            d = match.capturesdict()
+            prefix = fr"(?P<_{i}>body/\d+)"
+            repl = [fr"{d['SKIP'][0]}/{prefix}/{d['SUFFIX'][0]}"]
+            offset = " " * (len(r"body/\d+") + 1)
+            for suffix in match.captures("SUFFIXES"):
+                repl.append(fr"{d['SKIP'][0]}/(?P=_{i}){offset}/{suffix}")
+            self.s = self.s[: match.start()] + "".join(repl) + self.s[match.end() :]
+            self.s = self.s.replace(fr"?\1/{prefix}", fr" \1/{prefix}")
--- tests/test_suggest_regex.py
+++ tests/test_suggest_regex.py
@@ -16,7 +16,7 @@ sources = r"""
 /body/0/iter/args/0/func/id='len'
 /body/0/iter/keywords/length=0
 ---
-          ^(.*?)/_type='For'
+           ^(.*)/_type='For'
 \n(?:\1.+\n)*?\1/lineno=(?P<LINE>\d+)
 \n(?:\1.+\n)*?\1/iter/_type='Call'
 \n(?:\1.+\n)*?\1/iter/func/id='range'
@@ -25,7 +25,40 @@ sources = r"""
 \n(?:\1.+\n)*?\1/iter/args/0/func/id='len'
 \n(?:\1.+\n)*?\1/iter/keywords/length=0
 >>>
+
+<<< Capture and match a given value among ("name", "id", "_hash", "_ids")
+/body/0/_type='Assign'
+/body/0/lineno=1
+/body/0/targets/0/_hash=0x5c36d5a0
+/body/0/value/_type='UnaryOp'
+/body/0/value/op/_type='USub'
+/body/0/value/operand/_hash=0x5c36d5a0
+---
+           ^(.*)/_type='Assign'
+\n(?:\1.+\n)*?\1/lineno=(?P<LINE>\d+)
+\n(?:\1.+\n)*?\1/targets/0/_hash=(?P<HASH>.+) # capture _hash
+\n(?:\1.+\n)*?\1/value/_type='UnaryOp'
+\n(?:\1.+\n)*?\1/value/op/_type='USub'
+\n(?:\1.+\n)*?\1/value/operand/_hash=(?P=HASH) # match _hash
+>>>
+
+<<< Abstract bodies
+/body/0/_type='For'
+/body/0/lineno=1
+/body/0/iter/_hash=0x4a4b82fd
+/body/0/body/0/_type='For'
+/body/0/body/0/lineno=2
+/body/0/body/0/iter/_hash=0x4a4b82fd
+---
+           ^(.*)/_type='For'
+\n(?:\1.+\n)*?\1/lineno=(?P<LINE>\d+)
+\n(?:\1.+\n)*?\1/iter/_hash=(?P<HASH>.+) # capture _hash
+\n(?:\1.+\n)* \1/(?P<_1>body/\d+)/_type='For'
+\n(?:\1.+\n)*?\1/(?P=_1)         /lineno=(?P<LINE>\d+)
+\n(?:\1.+\n)*?\1/(?P=_1)         /iter/_hash=(?P=HASH) # match _hash
+>>>
 """
+
 source_rex = regex.compile(r"(?ms)^<<< ([^\n]+)\n(.+?\n)---\n(.+?\n)>>>")
 examples = [m for m in source_rex.findall(sources)]  # [-1:]
 suggestion = suggest_regex.Suggestion()
