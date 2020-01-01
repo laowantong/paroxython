71ffc62671bc036d4e1ccce815fd3491e3a6e9fc
laowantong <laowantong@users.noreply.github.com>
2020-01-01 16:16:45 +0100

    suppress deprecated code

--- helpers/draw_flow.py
+++ helpers/draw_flow.py
@@ -6,23 +6,22 @@ import regex
 paths = list(Path("paroxython").glob("*.py"))
 names = [str(path)[len("paroxython/") : -3] for path in paths]
 base = "docs/flow"
+find_all = regex.compile(fr"(?m)^from ({'|'.join(names)}) import").findall
 
 Path(f"{base}.dot").write_text(
-    "digraph G {\n    node [shape=box fontname=Courier style=filled fillcolor=cornsilk]\n%s\n}\n"
+    "digraph G {\nnode [shape=box fontname=Courier style=filled fillcolor=cornsilk]\n%s\n}\n"
     % "\n".join(
         [
-            '    "db.json" [shape=cylinder fillcolor=moccasin]',
-            '    "ref.md" [shape=note fillcolor=moccasin]',
-            '    make_db -> "db.json"',
-            '    "db.json" -> program_filter',
-            '    "ref.md" -> source_parser',
+            '"db.json" [shape=cylinder fillcolor=moccasin]',
+            '"ref.md" [shape=note fillcolor=moccasin]',
+            'make_db -> "db.json"',
+            '"db.json" -> program_filter',
+            '"ref.md" -> source_parser',
         ]
         + [
-            f"    {imported_name} -> {name}"
+            f"{imported_name} -> {name}"
             for (name, path) in zip(names, paths)
-            for imported_name in regex.findall(
-                fr"(?m)^from ({'|'.join(names)}) import", path.read_text()
-            )
+            for imported_name in find_all(path.read_text())
         ]
     )
 )
