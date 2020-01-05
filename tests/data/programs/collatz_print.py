1029756c3333a389e351663e08a658faf1d5cf87
laowantong <laowantong@users.noreply.github.com>
2020-01-05 20:27:23 +0100

    restore a hint for an existing label in collatz_print.py

--- tests/data/programs/collatz_print.py
+++ tests/data/programs/collatz_print.py
@@ -1,5 +1,5 @@
 def print_collatz(n):  # paroxython: added_block_label...
-    while n != 1:
+    while n != 1:  # paroxython: suggest_constant_definition
         print(n)
         if n % 2 == 0:  # paroxython: added_label_on_line_4
             n = n // 2  # paroxython: -literal:Num -binary_operator:FloorDiv
--- tests/test_generate_labels.py
+++ tests/test_generate_labels.py
@@ -20,7 +20,7 @@ def test_generate_labeled_sources():
     assert source == "\n".join(
         [
             "def print_collatz(n): # added_block_label (-> +7), function:print_collatz (-> +7)",
-            "    while n != 1: # comparison_operator:NotEq, evolve_state (-> +5), int_literal, literal:Num",
+            "    while n != 1: # comparison_operator:NotEq, evolve_state (-> +5), int_literal, literal:Num, suggest_constant_definition",
             "        print(n) # function_call:print",
             "        if n % 2 == 0: # added_label_on_line_4, binary_operator:Mod, comparison_operator:Eq, divisibility_test:2, if (-> +3), if_else (-> +3), int_literal, literal:Num, suggest_conditional_expression (-> +3)",
             "            n = n // 2 # assignment, int_literal, suggest_augmented_assignment",
