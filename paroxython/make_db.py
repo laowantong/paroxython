2dbb6bd725fa27aa5f44890c2b48a35c9f7de2b5
laowantong <laowantong@users.noreply.github.com>
2020-02-03 12:22:46 +0100

    refactor into a class

--- paroxython/make_db.py
+++ paroxython/make_db.py
@@ -37,87 +37,65 @@ LabelInfos = Dict[LabelName, List[ProgramName]]
 TaxonInfos = Dict[TaxonName, List[ProgramName]]
 
 
-class DB(TypedDict):
-    programs: ProgramInfos
-    labels: LabelInfos
-    taxons: TaxonInfos
+class Database:
+    def __init__(self, directories: List[str], *args, **kargs) -> None:
+        """collect all infos pertaining to the programs, the labels and the taxons."""
+        programs: List[Program] = []
+        for directory in directories:
+            programs.extend(generate_labelled_programs(directory, *args, **kargs))
 
+        taxonomy = Taxonomy()
+        paths_taxons = list(taxonomy(programs))
 
-def make_database(directories: List[str], *args, **kargs) -> DB:
-    """Serialize all infos pertaining to the programs, the labels and the taxons."""
-    programs: List[Program] = []
-    for directory in directories:
-        programs.extend(generate_labelled_programs(directory, *args, **kargs))
-    taxonomy = Taxonomy()
-    paths_taxons = list(taxonomy(programs))
-    db: DB = {
-        "programs": get_program_infos(programs),
-        "labels": get_label_infos(programs),
-        "taxons": get_taxon_infos(paths_taxons),
-    }
-    inject_labels(db, programs)
-    inject_taxons(db, paths_taxons)
-    return db
+        self.programs: ProgramInfos = {}
+        for program in programs:
+            self.programs[ProgramName(str(program.path))] = {
+                "timestamp": str(datetime.fromtimestamp(program.path.stat().st_mtime)),
+                "source": program.source,
+                "labels": {},  # to be populated by inject_labels()
+                "taxons": {},  # to be populated by inject_taxons()
+            }
 
+        self.labels: LabelInfos = defaultdict(list)
+        for program in programs:
+            for label in program.labels:
+                self.labels[label.name].append(ProgramName(str(program.path)))
+        for program in programs:
+            self.programs[ProgramName(str(program.path))]["labels"] = prepared(program.labels)
 
-def get_program_infos(programs: List[Program]) -> ProgramInfos:
-    result: ProgramInfos = {}
-    for program in programs:
-        result[ProgramName(str(program.path))] = {
-            "timestamp": str(datetime.fromtimestamp(program.path.stat().st_mtime)),
-            "source": program.source,
-            "labels": {},  # to be populated by inject_labels()
-            "taxons": {},  # to be populated by inject_taxons()
+        self.taxons: TaxonInfos = defaultdict(list)
+        for (path, taxons) in paths_taxons:
+            for taxon in taxons:
+                self.taxons[taxon.name].append(ProgramName(str(path)))
+        for (path, taxons) in paths_taxons:
+            self.programs[ProgramName(str(path))]["taxons"] = prepared(taxons)
+
+    def write_json(self, path: str) -> None:
+        """Dump the data to JSON, reduce each list of spans to one line, write it."""
+        data = {
+            "programs": self.programs,
+            "labels": self.labels,
+            "taxons": self.taxons,
         }
-    return result
-
-
-def get_label_infos(programs: List[Program]) -> LabelInfos:
-    result: LabelInfos = defaultdict(list)
-    for program in programs:
-        for label in program.labels:
-            result[label.name].append(ProgramName(str(program.path)))
-    return dict(result)
-
-
-def get_taxon_infos(paths_taxons: List[PathTaxons]) -> TaxonInfos:
-    result: TaxonInfos = defaultdict(list)
-    for (path, taxons) in paths_taxons:
-        for taxon in taxons:
-            result[taxon.name].append(ProgramName(str(path)))
-    return dict(result)
+        text = json.dumps(data, indent=2)
+        text = regex.sub(r"\s*\[\s+(\d+),\s+(\d+)\s+\](,?)\s+", r"[\1,\2]\3", text)
+        Path(path).write_text(text)
 
 
 # fmt: off
 @overload
-def serialized(tags: Labels) -> LabelsSpans:
+def prepared(tags: Labels) -> LabelsSpans:
     ...  # pragma: no cover
 @overload
-def serialized(tags: Taxons) -> TaxonsSpans:
+def prepared(tags: Taxons) -> TaxonsSpans:
     ...  # pragma: no cover
-def serialized(tags):
+def prepared(tags):
+    """Prepare the spans for serialization."""
     result: Any = {}
     for (tag_name, spans) in tags:
         result[tag_name] = [span.to_couple() for span in sorted(set(spans))]
     return result
 # fmt: on
-
-
-def inject_labels(db: DB, programs: List[Program]) -> None:
-    for program in programs:
-        db["programs"][ProgramName(str(program.path))]["labels"] = serialized(program.labels)
-
-
-def inject_taxons(db: DB, paths_taxons: List[PathTaxons]) -> None:
-    for (path, taxons) in paths_taxons:
-        db["programs"][ProgramName(str(path))]["taxons"] = serialized(taxons)
-
-
-def to_json(db: DB) -> str:
-    """Convert the DB into JSON and reduce to one line each list of spans."""
-    text = json.dumps(db, indent=2)
-    text = regex.sub(r"\s*\[\s+(\d+),\s+(\d+)\s+\](,?)\s+", r"[\1,\2]\3", text)
-    return text
 
 
 if __name__ == "__main__":
@@ -126,5 +104,5 @@ if __name__ == "__main__":
         # "../Python/maths",
         # "../Algo/programs"
     ]
-    db = make_database(directories)
-    Path("db.json").write_text(to_json(db))
+    database = Database(directories)
+    database.write_json("db.json")
