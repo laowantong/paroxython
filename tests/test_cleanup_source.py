722ad986b7545026816e016f5dff9dcdfccf5a20
laowantong <laowantong@users.noreply.github.com>
2020-05-19 13:34:30 +0200

    try to find Python 3.5 build error

--- tests/test_cleanup_source.py
+++ tests/test_cleanup_source.py
@@ -177,6 +177,7 @@ def test_full_cleaning(title, original, expected):
     print(title)
     result = cleanup(original)
     print(result)
+    print(r"\s*".join(regex.split(r"\b", HINT_COMMENT.replace(" ", ""))) + r"\s*")
     assert result == expected
 
 
