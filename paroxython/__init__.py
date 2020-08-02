laowantong/paroxython

commit bf9dda8c8f60543f3432d91444f3b7a93cc920e5

calculate SLOC stats as part of the doc generation

paroxython/__init__.py
@@ -63,6 +63,7 @@ if "ipykernel" in sys.modules:
 
     @magics_class
     class ParoxythonMagics(Magics):
+        @staticmethod
         @line_cell_magic
         def paroxython(self, line, source=None):
             """

docs/cli/cli.html
@@ -73,7 +73,7 @@ def main():
     else:  # pragma: no cover
         version = distribution.version
     args = docopt(__doc__, version=f&#34;paroxython version {version}&#34;, options_first=True)
-    if not args.get(&#34;COMMAND&#34;):
+    if not args.get(&#34;COMMAND&#34;):  # pragma: no cover
         sys.exit(__doc__)
     command_name = args[&#34;COMMAND&#34;].lower()
     command_args = [command_name] + args[&#34;ARGS&#34;]
@@ -115,7 +115,7 @@ if __name__ == &#34;__main__&#34;:
     else:  # pragma: no cover
         version = distribution.version
     args = docopt(__doc__, version=f&#34;paroxython version {version}&#34;, options_first=True)
-    if not args.get(&#34;COMMAND&#34;):
+    if not args.get(&#34;COMMAND&#34;):  # pragma: no cover
         sys.exit(__doc__)
     command_name = args[&#34;COMMAND&#34;].lower()
     command_args = [command_name] + args[&#34;ARGS&#34;]

docs/make_db.html
@@ -149,7 +149,7 @@ class Database:
                         )
                     )
 
-        if db_path.exists():  # Python 3.8: use missing_ok=True parameter
+        if db_path.exists():  # pragma: no cover / Python 3.8: use missing_ok=True parameter
             db_path.unlink()
         connexion = sqlite3.connect(str(db_path))  # str() for Python 3.6 compatibility
         c = connexion.cursor()
@@ -669,7 +669,7 @@ simple couples of integers.</p>
                         )
                     )
 
-        if db_path.exists():  # Python 3.8: use missing_ok=True parameter
+        if db_path.exists():  # pragma: no cover / Python 3.8: use missing_ok=True parameter
             db_path.unlink()
         connexion = sqlite3.connect(str(db_path))  # str() for Python 3.6 compatibility
         c = connexion.cursor()
@@ -1002,7 +1002,7 @@ Paroxython.</p></div>
                     )
                 )
 
-    if db_path.exists():  # Python 3.8: use missing_ok=True parameter
+    if db_path.exists():  # pragma: no cover / Python 3.8: use missing_ok=True parameter
         db_path.unlink()
     connexion = sqlite3.connect(str(db_path))  # str() for Python 3.6 compatibility
     c = connexion.cursor()

helpers/generate_pdoc.py
@@ -8,6 +8,7 @@ import regex  # type: ignore
 import context
 from paroxython.cli.cli_tag import main as tag_program
 from paroxython.goodies import add_line_numbers
+from paroxython.preprocess_source import Cleanup
 
 PATH = f"{Path(dirname(__file__)).parent}"
 
@@ -152,6 +153,29 @@ def insert_line_breaks():
         path.write_text(source)
 
 
+def compute_stats():
+    readme_path = Path("README.md")
+    readme_text = readme_path.read_text()
+    cleanup = Cleanup("full")
+    directories = ["paroxython", "tests", "helpers"]
+    for directory in directories:
+        total = 0
+        for program_path in Path(directory).glob("**/*.py"):
+            source = program_path.read_text()
+            # Work around a weird error:
+            # tokenize.TokenError: ('EOF in multi-line string', (12, 10))
+            source = source.replace('if __name__ == "__main__":\n    bar = foo', "pass\npass")
+            source = cleanup.run(source)
+            total += source.count("\n")
+        print(f"{directory}: {total} SLOC")
+        total = 100 * round(total / 100)
+        (readme_text, n) = regex.subn(
+            fr"(?m)^(!\[{directory} SLOC\].+?)~\d+(%20SLOC)", fr"\1~{total}\2", readme_text
+        )
+        assert n > 0, f"Unable to create badge for '{directory}' SLOC."
+    readme_path.write_text(readme_text)
+
+
 def main():
     update_readme_example()
     generate_html()
@@ -161,6 +185,7 @@ def main():
     embed_code_with_line_numbers()
     cleanup_index()
     insert_line_breaks()
+    compute_stats()
 
 
 if __name__ == "__main__":

paroxython/cli/cli.py
@@ -40,7 +40,7 @@ def main():
     else:  # pragma: no cover
         version = distribution.version
     args = docopt(__doc__, version=f"paroxython version {version}", options_first=True)
-    if not args.get("COMMAND"):
+    if not args.get("COMMAND"):  # pragma: no cover
         sys.exit(__doc__)
     command_name = args["COMMAND"].lower()
     command_args = [command_name] + args["ARGS"]

paroxython/make_db.py
@@ -298,7 +298,7 @@ class Database:
                         )
                     )
 
-        if db_path.exists():  # Python 3.8: use missing_ok=True parameter
+        if db_path.exists():  # pragma: no cover / Python 3.8: use missing_ok=True parameter
             db_path.unlink()
         connexion = sqlite3.connect(str(db_path))  # str() for Python 3.6 compatibility
         c = connexion.cursor()

tests/test_cleanup_source.py
@@ -161,7 +161,7 @@ fizz = [
 <<< `if __name__ == "__main__":` part is suppressed.
 foo = bar
 if __name__ == "__main__":
-    bar == foo
+    bar = foo
 ---
 foo = bar
 >>>

commit 8834eed7f16b1cec9e458f28a782d4f9b8900bee

fix Jupyter magic command (regression introduced in bf9dda8)

.coveragerc
@@ -8,6 +8,7 @@ omit =
     helpers/reformat_spec.py
     helpers/make_programming_idioms_folder.py
     helpers/print_taxon_patterns.py
+    helpers/parse_source.py
     paroxython/__init__.py
     paroxython/make_snapshots.py
     paroxython/compare_spans.py

docs/cli/cli_collect.html
@@ -53,7 +53,7 @@
 --no_timestamp      Don't store programs' last modification date.
 -t --taxonomy=PATH  The path of a TSV file mapping labels onto taxa.
                     If not specified, use the included default taxonomy:
-                    &lt;https://github.com/laowantong/paroxython/blob/0.4.0/paroxython/resources/taxonomy.tsv&gt;
+                    &lt;https://github.com/laowantong/paroxython/blob/0.4.1/paroxython/resources/taxonomy.tsv&gt;
 </code></pre>
 <details class="source">
 <summary>

docs/cli/cli_tag.html
@@ -36,7 +36,7 @@
 -l --labels         Output the labels instead of the taxa.
 -t --taxonomy=PATH  The path of a TSV file mapping labels onto taxa.
                     If not specified, use the included default taxonomy:
-                    &lt;https://github.com/laowantong/paroxython/blob/0.4.0/paroxython/resources/taxonomy.tsv&gt;
+                    &lt;https://github.com/laowantong/paroxython/blob/0.4.1/paroxython/resources/taxonomy.tsv&gt;
 </code></pre>
 <details class="source">
 <summary>

paroxython/__init__.py
@@ -29,7 +29,7 @@ if "ipykernel" in sys.modules:
     class ParoxythonMagics(Magics):
         @staticmethod
         @line_cell_magic
-        def paroxython(self, line, source=None):
+        def paroxython(line, source=None):
             """
             Tag a Python code cell and output the table of its taxa or labels.
 

paroxython/cli/cli_collect.py
@@ -29,7 +29,7 @@ OPTIONS:
     --no_timestamp      Don't store programs' last modification date.
     -t --taxonomy=PATH  The path of a TSV file mapping labels onto taxa.
                         If not specified, use the included default taxonomy:
-                        https://github.com/laowantong/paroxython/blob/0.4.0/paroxython/resources/taxonomy.tsv
+                        https://github.com/laowantong/paroxython/blob/0.4.1/paroxython/resources/taxonomy.tsv
     ```
 """
 

paroxython/cli/cli_tag.py
@@ -12,7 +12,7 @@ OPTIONS:
     -l --labels         Output the labels instead of the taxa.
     -t --taxonomy=PATH  The path of a TSV file mapping labels onto taxa.
                         If not specified, use the included default taxonomy:
-                        https://github.com/laowantong/paroxython/blob/0.4.0/paroxython/resources/taxonomy.tsv
+                        https://github.com/laowantong/paroxython/blob/0.4.1/paroxython/resources/taxonomy.tsv
     ```
 """
 

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "paroxython"
-version = "0.4.0"
+version = "0.4.1"
 description = "Search Python code for algorithmic features"
 authors = ["Aristide Grange"]
 license = "MIT"